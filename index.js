98ac35cb007b8141cb8910c3ca39a8171ecf8eae
Marco Munizaga <marco@stripe.com>
2020-02-06 17:12:51 -0800

    First working version

--- index.js
+++ index.js
@@ -1,5 +1,216 @@
 // An implementation of https://github.com/rust-lang/datafrog
+const _ = require("lodash");
 
+const sortByFirstAsKey = ([k]) => k;
+
+// A sorted list of distinct tuples.
 class Relation {
-  elements = [];
+  constructor(fromArray, sortByFn = sortByFirstAsKey) {
+    this.sortByFn = sortByFn;
+    this.elements = _.uniqWith(_.sortBy(fromArray, sortByFn), _.isEqual);
+  }
+
+  merge(otherRelation) {
+    if (otherRelation.sortByFn !== this.sortByFn) {
+      throw new Error(
+        "Merging a relation that doesn't have the same sortByFn!"
+      );
+    }
+
+    return new Relation(
+      this.elements.concat(otherRelation.elements),
+      this.sortByFn
+    );
+  }
+
+  get length() {
+    return this.elements.length;
+  }
 }
+
+class Variable {
+  constructor() {
+    // A list of already processed tuples.
+    this.stable = [];
+    // A list of recently added but unprocessed tuples.
+    this.recent = new Relation([]);
+    // A list of tuples yet to be introduced.
+    this.toAdd = [];
+  }
+
+  insert(relation) {
+    this.toAdd.push(relation);
+  }
+
+  fromJoin(otherVariable, logicFn) {
+    joinInto(this, otherVariable, this, logicFn);
+  }
+
+  changed() {
+    // 1. Merge this.recent into this.stable.
+    if (this.recent.elements.length > 0) {
+      let recent = this.recent;
+      this.recent = new Relation([], recent.sortByFn);
+
+      // TODO visualize this!
+      // Merge smaller stable relations into our recent one. This keeps bigger
+      // relations to the left, and smaller relations to the right. merging them
+      // over time so not to keep a bunch of small relations.
+      while (
+        this.stable[this.stable.length - 1] &&
+        this.stable[this.stable.length - 1].length <= 2 * recent.elements.length
+      ) {
+        const last = this.stable.pop();
+        recent = last.merge(recent);
+      }
+
+      this.stable.push(recent);
+    }
+
+    // 2. Move this.to_add into this.recent.
+    if (this.toAdd.length > 0) {
+      // 2a. Merge all newly added relations.
+      let toAdd = this.toAdd.pop();
+      while (this.toAdd.length > 0) {
+        toAdd = toAdd.merge(this.toAdd.pop());
+      }
+
+      // 2b. Restrict `to_add` to tuples not in `this.stable`.
+      for (let index = 0; index < this.stable.length; index++) {
+        const stableRelation = this.stable[index];
+        toAdd.elements = toAdd.elements.filter(elem => {
+          let searchIdx = gallop(stableRelation.elements, ([k]) => k < elem[0]);
+
+          while (
+            searchIdx < stableRelation.elements.length &&
+            stableRelation.elements[searchIdx][0] === elem[0]
+          ) {
+            if (_.isEqual(stableRelation.elements[searchIdx], elem)) {
+              return false;
+            }
+            searchIdx++;
+          }
+          return true;
+        });
+      }
+      this.recent = toAdd;
+    }
+
+    // Return true iff recent is non-empty.
+    return !!this.recent.length;
+  }
+}
+
+// Finds the first index for which predicate is false. Returns an index of array.length if it will never be true
+// predFn takes the form of (x: number) => boolean
+function gallop(array, predFn, startIdx = 0) {
+  if (array.length - startIdx <= 0 || !predFn(array[startIdx])) {
+    return 0;
+  }
+
+  let step = 1;
+
+  // Step up until we've seen a false result from predFn
+  while (startIdx + step < array.length && predFn(array[startIdx + step])) {
+    startIdx += step;
+    step = step << 1;
+  }
+
+  // Now step down until we get a false result
+  step = step >> 1;
+  while (step > 0) {
+    if (startIdx + step < array.length && predFn(array[startIdx + step])) {
+      startIdx += step;
+    }
+    step = step >> 1;
+  }
+
+  return startIdx + 1;
+}
+
+// logicFn takes the form of (key, val1, val2)
+// relations should be a sorted set of (K, V) tuples, sorted by key.
+// we join on the first item in the tuple.
+function joinHelper(relationA, relationB, logicFn) {
+  // Keep track of the indices into the relation's elements
+  let idxA = 0;
+  let idxB = 0;
+  let max = 0;
+  while (
+    max < 100 &&
+    idxA < relationA.elements.length &&
+    idxB < relationB.elements.length
+  ) {
+    max++;
+    let elemAKey = relationA.elements[idxA][0];
+    let elemBKey = relationB.elements[idxB][0];
+
+    if (elemAKey < elemBKey) {
+      // We have to move idxA up to catch to elemB
+      idxA = gallop(relationA.elements, ([k]) => k < elemBKey, idxA);
+    } else if (elemBKey < elemAKey) {
+      // We have to move idxB up to catch to elemA
+      idxB = gallop(relationB.elements, ([k]) => k < elemAKey, idxB);
+    } else {
+      // They're equal. We have our join
+
+      // Figure out the count of matches in each relation
+      const matchingCountA = _.takeWhile(
+        relationA.elements.slice(idxA),
+        ([k]) => k === elemAKey
+      ).length;
+      const matchingCountB = _.takeWhile(
+        relationB.elements.slice(idxB),
+        ([k]) => k === elemAKey
+      ).length;
+
+      // Call logicFn on the cross product
+      for (let i = 0; i < matchingCountA; i++) {
+        for (let j = 0; j < matchingCountB; j++) {
+          logicFn(
+            elemAKey,
+            relationA.elements[idxA + i][1],
+            relationB.elements[idxB + j][1]
+          );
+        }
+      }
+
+      idxA += matchingCountA;
+      idxB += matchingCountB;
+    }
+  }
+}
+
+// logicFn is of the type: (Key, ValA, ValB) => Result
+// where Result is the type of data that will live in outputVariable.
+// To join these two variables we have to join 3 things.
+// inputVariableA.recent – inputVariableB.stable
+// inputVariableA.stable – inputVariableB.recent
+// inputVariableA.recent – inputVariableB.recent
+function joinInto(inputVariableA, inputVariableB, outputVariable, logicFn) {
+  const results = [];
+
+  // inputVariableA.recent – inputVariableB.stable
+  for (let index = 0; index < inputVariableB.stable.length; index++) {
+    const stableRelation = inputVariableB.stable[index];
+    joinHelper(inputVariableA.recent, stableRelation, (k, vA, vB) =>
+      results.push(logicFn(k, vA, vB))
+    );
+  }
+  // inputVariableA.stable – inputVariableB.recent
+  for (let index = 0; index < inputVariableA.stable.length; index++) {
+    const stableRelation = inputVariableA.stable[index];
+    joinHelper(stableRelation, inputVariableB.recent, (k, vA, vB) =>
+      results.push(logicFn(k, vA, vB))
+    );
+  }
+
+  // inputVariableA.recent – inputVariableB.recent
+  joinHelper(inputVariableA.recent, inputVariableB.recent, (k, vA, vB) =>
+    results.push(logicFn(k, vA, vB))
+  );
+
+  outputVariable.insert(new Relation(results));
+}
+
+module.exports = { Relation, Variable, gallop, joinHelper };
--- index.test.js
+++ index.test.js
@@ -1,7 +1,187 @@
-describe("Sum numbers", () => {
-  test("it should sum two numbers correctly", () => {
-    const sum = 1 + 2;
-    const expectedResult = 3;
-    expect(sum).toEqual(expectedResult);
+const { Relation, Variable, gallop, joinHelper } = require("./index.js");
+
+const sortByIdentity = a => a;
+
+describe("Relation", () => {
+  test("Create should sort + dedup", () => {
+    const relation = new Relation(
+      [1, 5, 5, 0, 1, 2, 3, 10, 19, 20],
+      sortByIdentity
+    );
+    const expectedResult = [0, 1, 2, 3, 5, 10, 19, 20];
+    expect(relation.elements).toEqual(expectedResult);
+  });
+
+  test("Merging should sort + dedup", () => {
+    const relationA = new Relation([1, 5, 5, 0, 1, 2, 3], sortByIdentity);
+    const relationB = new Relation([5, 2, 2, 6, 0, 9, 2], sortByIdentity);
+    const expectedResult = [0, 1, 2, 3, 5, 6, 9];
+    expect(relationA.merge(relationB).elements).toEqual(expectedResult);
+  });
+
+  test("Should dedup tuples", () => {
+    let relation = new Relation([[1, "bob"]]);
+    expect(relation.elements).toEqual([[1, "bob"]]);
+    relation = relation.merge(new Relation([[1, "bob"]]));
+    expect(relation.elements).toEqual([[1, "bob"]]);
+  });
+});
+
+describe("gallop", () => {
+  test("Should find the first index for an empty array", () => {
+    const array = [];
+    expect(gallop(array, n => false)).toEqual(0);
+  });
+  test("Should find the first index", () => {
+    const array = [0, 1, 2, 3, 4, 5];
+    expect(gallop(array, n => false)).toEqual(0);
+  });
+
+  test("Should return an index out of bounds", () => {
+    const array = [0, 1, 2, 3, 4, 5];
+    expect(gallop(array, n => true)).toEqual(array.length);
+  });
+
+  test("Should find the first index > 3", () => {
+    const array = [0, 1, 2, 3, 4, 5];
+    expect(gallop(array, n => n < 3)).toEqual(3);
+  });
+
+  test("Should find the first index > 3", () => {
+    const array = [0, 1, 2, 3, 3, 3, 4, 5];
+    expect(gallop(array, n => n < 3)).toEqual(3);
+  });
+});
+
+describe("joinHelper", () => {
+  test("Should find a simple join", () => {
+    const relationA = new Relation([[1, "hi"]]);
+    const relationB = new Relation([[1, "bob"]]);
+    const output = [];
+
+    joinHelper(relationA, relationB, (k, valueA, valueB) =>
+      output.push([k, valueA, valueB])
+    );
+
+    const expectedResult = [[1, "hi", "bob"]];
+
+    expect(output).toEqual(expectedResult);
+  });
+  test("Should find joins", () => {
+    const relationA = new Relation([
+      [1, "hi"],
+      [2, "hello"],
+      [2, "goodbye"],
+      [1, "greetings"]
+    ]);
+    const relationB = new Relation([
+      [1, "bob"],
+      [2, "world"],
+      [3, "sarah"]
+    ]);
+    const output = [];
+
+    joinHelper(relationA, relationB, (k, valueA, valueB) =>
+      output.push([k, valueA, valueB])
+    );
+
+    const expectedResult = [
+      [1, "hi", "bob"],
+      [1, "greetings", "bob"],
+      [2, "hello", "world"],
+      [2, "goodbye", "world"]
+    ];
+
+    expect(output).toEqual(expectedResult);
+  });
+});
+
+describe("Variable", () => {
+  test("Should move inner relations along", () => {
+    const relation = new Relation([
+      [1, "bob"],
+      [2, "world"],
+      [3, "sarah"]
+    ]);
+
+    const variable = new Variable();
+    variable.insert(relation);
+
+    expect(variable.stable).toEqual([]);
+    expect(variable.recent.elements).toEqual([]);
+    expect(variable.toAdd).toEqual([relation]);
+
+    // query changed?
+    let hasChanged = variable.changed();
+    expect(hasChanged).toEqual(true);
+    expect(variable.stable).toEqual([]);
+    expect(variable.recent).toEqual(relation);
+    expect(variable.toAdd).toEqual([]);
+
+    hasChanged = variable.changed();
+    expect(hasChanged).toEqual(false);
+    expect(variable.stable).toEqual([relation]);
+    expect(variable.recent.elements).toEqual([]);
+    expect(variable.toAdd).toEqual([]);
+  });
+
+  test("Should remove entries already in stable from toAdd", () => {
+    const relation = new Relation([[1, "bob"]]);
+
+    const variable = new Variable();
+    variable.insert(relation);
+    while (variable.changed()) {}
+
+    expect(variable.stable[0]).toEqual(relation);
+    expect(variable.recent.elements).toEqual([]);
+    expect(variable.toAdd).toEqual([]);
+
+    variable.insert(new Relation([[1, "bob"]]));
+    while (variable.changed()) {}
+
+    expect(variable.stable[0]).toEqual(relation);
+    expect(variable.recent.elements).toEqual([]);
+    expect(variable.toAdd).toEqual([]);
+  });
+
+  test("Join from another variable", () => {
+    const relationA = new Relation([
+      [1, "hi"],
+      [2, "hello"],
+      [2, "goodbye"],
+      [1, "greetings"]
+    ]);
+    const relationB = new Relation([
+      [1, "bob"],
+      [2, "world"],
+      [3, "sarah"]
+    ]);
+
+    const variableA = new Variable();
+    const variableB = new Variable();
+
+    variableA.insert(relationA);
+    variableB.insert(relationB);
+    while (variableB.changed()) {}
+
+    let max = 0;
+    while (variableA.changed() && max++ < 100) {
+      variableA.fromJoin(variableB, (k, vA, vB) => {
+        return [k, vB];
+      });
+    }
+
+    expect(variableA.recent.elements).toEqual([]);
+    expect(variableA.toAdd).toEqual([]);
+
+    expect(variableA.stable.length).toEqual(1);
+    expect(variableA.stable[0].elements).toEqual([
+      [1, "hi"],
+      [1, "greetings"],
+      [1, "bob"],
+      [2, "hello"],
+      [2, "goodbye"],
+      [2, "world"]
+    ]);
   });
 });
